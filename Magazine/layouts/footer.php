267149e7cd7352909272c4015195463cb36c2c22
piotrkunicki <piotrkunicki@gavick.com>
2014-12-22 14:30:26 +0100

    Magazine 1.8 ready to release! (based on GavernWP 1.12)

--- Magazine/layouts/footer.php
+++ Magazine/layouts/footer.php
@@ -19,7 +19,7 @@
 			<?php gavern_menu('footermenu', 'gk-footer-menu'); ?>
 			
 			<div class="gk-copyrights">
-				Free <a href="http://www.gavick.com/wordpress-themes.html" title="WordPress Themes">WordPress Theme</a> designed by <a href="http://www.gavick.com">GavickPro</a>
+				Free <a href="http://www.gavick.com/wordpress-themes" title="Best colllection of WordPress Themes">WordPress Theme</a> designed by <a href="http://www.gavick.com">GavickPro</a>
 			</div>
 			
 			<?php if(get_option($tpl->name . '_template_footer_logo', 'Y') == 'Y') : ?>
